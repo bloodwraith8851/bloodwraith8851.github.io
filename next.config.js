238109d651a5bc89f13f6eedf24e5d447d64d503
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-06-02 21:17:05 +0530

    Update GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  assetPrefix: '.',
+  trailingSlash: true,
+  distDir: 'dist',
   // If your repository name is different from your GitHub username,
   // uncomment and update the following line:
   // basePath: '/your-repo-name',
