89dcf7c6df8052b7fecac3075e385fedf8f77ee3
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-06-03 09:04:58 +0530

    Fix configuration for standalone mode and proper file handling

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
   basePath: '',
-  assetPrefix: '',
+  assetPrefix: '.',
   trailingSlash: true,
 }
 
