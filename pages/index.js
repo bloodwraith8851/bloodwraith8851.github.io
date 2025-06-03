c9142a7fec38281a75dcd2912c8e9e5b7142c7da
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-06-03 08:59:08 +0530

    Switch to static export and fix index page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Ubuntu from "../components/ubuntu";
 import ReactGA from 'react-ga4';
 import Meta from "../components/SEO/Meta";
+import { useEffect } from 'react';
 
 const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID);
 
-function App() {
+export default function Home() {
+  useEffect(() => {
+    if (TRACKING_ID) {
+      ReactGA.initialize(TRACKING_ID);
+    }
+  }, []);
+
   return (
     <>
       <Meta />
       <Ubuntu />
     </>
-  )
+  );
 }
-
-export default App;
