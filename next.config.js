52ca86cb19ac936886728240172f54866c47f7eb
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-06-03 08:39:39 +0530

    Update deployment configuration to use static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '.',
   basePath: '',
+  assetPrefix: './',
   trailingSlash: true,
 }
 
