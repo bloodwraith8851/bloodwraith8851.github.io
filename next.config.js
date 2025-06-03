69d7a7f3028b6bae88b3219e52939e15607d36ef
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-06-03 09:33:23 +0530

    Proper static export configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: '.',
+  basePath: process.env.NODE_ENV === 'production' ? '/bloodwraith8851.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/bloodwraith8851.github.io/' : '',
   trailingSlash: true,
 }
 
